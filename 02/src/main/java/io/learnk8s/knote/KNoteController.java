7dfdc5fdedb5306e34b4ce9e89465f471fc5186a
Ubuntu <ubuntu@ip-172-31-3-70.ap-south-1.compute.internal>
2023-11-19 21:43:25 +0000

    S3 Upload Error Fixes

--- 02/src/main/java/io/learnk8s/knote/KNoteController.java
+++ 02/src/main/java/io/learnk8s/knote/KNoteController.java
@@ -1,7 +1,8 @@
 package io.learnk8s.knote;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -11,9 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.core.sync.RequestBody;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
@@ -21,17 +26,23 @@ import java.util.UUID;
 @Controller
 public class KNoteController {
 
-    private final NotesRepository notesRepository;
-    private final AmazonS3 s3Client;
-    private final String bucketName;
-
     @Autowired
-    public KNoteController(NotesRepository notesRepository, AmazonS3 s3Client,
-                           @Value("${aws.s3.bucketName}") String bucketName) {
-        this.notesRepository = notesRepository;
-        this.s3Client = s3Client;
-        this.bucketName = bucketName;
-    }
+    private NotesRepository notesRepository;
+
+    @Value("${aws.s3.region}")
+    private String s3Region;
+    
+    @Value("${aws.accessKeyId}")
+    private String awsId;
+
+    @Value("${aws.secretKey}")
+    private String awsKey;
+
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
+    private final Parser parser = Parser.builder().build();
+    private final HtmlRenderer renderer = HtmlRenderer.builder().build();
 
     @GetMapping("/")
     public String index(Model model) {
@@ -39,21 +50,28 @@ public class KNoteController {
         return "index";
     }
 
-    @GetMapping("/notes")
-    public String displayNotes(Model model) {
-        getAllNotes(model);
-        return "notes";
-    }
-
     @PostMapping("/note")
-    public String saveNote(@RequestParam("image") MultipartFile file,
-                           @RequestParam String description,
-                           Model model) throws IOException {
-        if (!file.isEmpty()) {
-            uploadImage(file, description, model);
+    public String saveNotes(@RequestParam("image") MultipartFile file,
+                            @RequestParam String description,
+                            @RequestParam(required = false) String publish,
+                            @RequestParam(required = false) String upload,
+                            Model model) throws Exception {
+
+        if (publish != null && "Publish".equals(publish)) {
+            saveNote(description, model);
+            getAllNotes(model);
+            return "redirect:/";
         }
-        saveNote(description);
-        return "redirect:/notes";
+        if (upload != null && "Upload".equals(upload)) {
+            if (file != null && !file.getOriginalFilename().isEmpty()) {
+                String imageUrl = uploadImageToS3(file);
+                model.addAttribute("description",
+                        description + " ![](" + imageUrl + ")");
+            }
+            getAllNotes(model);
+            return "index";
+        }
+        return "index";
     }
 
     private void getAllNotes(Model model) {
@@ -62,32 +80,33 @@ public class KNoteController {
         model.addAttribute("notes", notes);
     }
 
-    private void uploadImage(MultipartFile file, String description, Model model) throws IOException {
-        String fileName = generateFileName(file);
-        File fileObj = convertMultiPartFileToFile(file);
-        s3Client.putObject(new PutObjectRequest(bucketName, fileName, fileObj));
-        fileObj.delete(); // Delete the local file after upload
+    private String uploadImageToS3(MultipartFile file) throws Exception {
+        String fileId = UUID.randomUUID().toString() + "." +
+                file.getOriginalFilename().split("\\.")[1];
+        String fileName = "uploads/" + fileId; // Using 'uploads/' as a folder in S3
 
-        String imageUrl = "https://" + bucketName + ".s3.amazonaws.com/" + fileName;
-        description += " ![](" + imageUrl + ")";
-        model.addAttribute("description", description);
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(awsId, awsKey);
+        S3Client s3 = S3Client.builder()
+                    .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                    .region(Region.of(s3Region)) // Use the region from the properties file
+                    .build();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                                                .bucket(bucketName)
+                                                .key(fileName)
+                                                .build();
+
+        s3.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
+
+        return "https://s3." + s3Region + ".amazonaws.com/" + bucketName + "/" + fileName;
     }
 
-    private File convertMultiPartFileToFile(MultipartFile file) throws IOException {
-        File convertedFile = new File(System.getProperty("java.io.tmpdir") + "/" + file.getOriginalFilename());
-        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
-            fos.write(file.getBytes());
+    private void saveNote(String description, Model model) {
+        if (description != null && !description.trim().isEmpty()) {
+            Node document = parser.parse(description.trim());
+            String html = renderer.render(document);
+            notesRepository.save(new Note(null, html));
+            model.addAttribute("description", "");
         }
-        return convertedFile;
-    }
-
-    private String generateFileName(MultipartFile multiPart) {
-        return UUID.randomUUID() + "_" + multiPart.getOriginalFilename().replace(" ", "_");
-    }
-
-    private void saveNote(String description) {
-        Note note = new Note();
-        note.setDescription(description);
-        notesRepository.save(note);
     }
 }
